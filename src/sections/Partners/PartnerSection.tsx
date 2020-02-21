fc18e74368a815d36a0b23330e2acbf78410997f
LeonY1 <leonyyuan@gmail.com>
2020-02-21 01:52:47 -0600

    Added HEB and USAA as Partners

--- src/sections/Partners/PartnerSection.tsx
+++ src/sections/Partners/PartnerSection.tsx
@@ -1,211 +1,207 @@
-import React from "react";
-import SectionBlock from "../../components/SectionDividers/SectionBlock";
+import React from 'react';
+import SectionBlock from '../../components/SectionDividers/SectionBlock';
 import {
-  GoldPartnerImage,
-  SilverPartnerImage,
-  BronzePartnerImage,
-  OtherPartnerImage,
-  PartnerContentWrapper,
-  PartnerTitleWrapper,
-  GoldPartnerContainer,
-  SilverPartnerContainer,
-  BronzePartnerContainer,
-  OtherPartnerContainer
-} from "./PartnerStyle";
+	GoldPartnerImage,
+	SilverPartnerImage,
+	BronzePartnerImage,
+	OtherPartnerImage,
+	PartnerContentWrapper,
+	PartnerTitleWrapper,
+	GoldPartnerContainer,
+	SilverPartnerContainer,
+	BronzePartnerContainer,
+	OtherPartnerContainer
+} from './PartnerStyle';
 
 function PartnerSection() {
-  return (
-    <SectionBlock sectionNumber={4} id="partners">
-      {{
-        content: PartnerContent
-      }}
-    </SectionBlock>
-  );
+	return (
+		<SectionBlock sectionNumber={4} id="partners">
+			{{
+				content: PartnerContent
+			}}
+		</SectionBlock>
+	);
 }
 
 const goldPartners = [
-  { name: "FrostBank", link: "https://www.frostbank.com/" },
-  { name: "ManTech", link: "https://www.mantech.com/", paddingTop: 30 },
-  { name: "TechData", link: "https://www.techdata.com/", paddingTop: 30 },
-  { name: "AFCS", link: "http://www.afciviliancareers.com/" },
-  { name: "iHeartMedia", link: "https://www.iheartmedia.com/", paddingTop: 30 }
+	{ name: 'FrostBank', link: 'https://www.frostbank.com/' },
+	{ name: 'ManTech', link: 'https://www.mantech.com/', paddingTop: 30 },
+	{ name: 'TechData', link: 'https://www.techdata.com/', paddingTop: 30 },
+	{ name: 'AFCS', link: 'http://www.afciviliancareers.com/' },
+	{ name: 'iHeartMedia', link: 'https://www.iheartmedia.com/', paddingTop: 30 },
+	{ name: 'HEB', link: 'https://www.heb.com/', paddingTop: 30 }
 ];
 
 const silverPartners = [
-  { name: "Accenture", link: "https://www.accenture.com/us-en" },
-  { name: "UTSA_CS", link: "https://cs.utsa.edu/" },
-  {
-    name: "Google",
-    link: "https://careers.google.com/students/"
-  }
+	{ name: 'Accenture', link: 'https://www.accenture.com/us-en' },
+	{ name: 'UTSA_CS', link: 'https://cs.utsa.edu/' },
+	{
+		name: 'Google',
+		link: 'https://careers.google.com/students/'
+	},
+	{
+		name: 'USAA',
+		link: 'https://www.usaa.com/'
+	}
 ];
 
 const bronzePartners = [
-  {
-    name: "CivTechSA",
-    fileType: "png",
-    link: "https://www.civtech-sa.com/",
-    bgColor: "#ffffff"
-  },
-  {
-    name: "Facebook",
-    fileType: "png",
-    link: "https://www.facebook.com/",
-    bgColor: "#ffffff"
-  },
+	{
+		name: 'CivTechSA',
+		fileType: 'png',
+		link: 'https://www.civtech-sa.com/',
+		bgColor: '#ffffff'
+	},
+	{
+		name: 'Facebook',
+		fileType: 'png',
+		link: 'https://www.facebook.com/',
+		bgColor: '#ffffff'
+	},
 
-  {
-    name: "UTSA_COS",
-    fileType: "png",
-    link: "https://www.utsa.edu/sciences/",
-    bgColor: "#ffffff"
-  },
-  {
-    name: "Valero",
-    fileType: "png",
-    link: "https://www.valero.com/en-us",
-    bgColor: "#ffffff"
-  },
-  {
-    name: "Webhead",
-    fileType: "png",
-    link: "https://www.webheadtech.com/",
-    bgColor: "#ffffff"
-  },
-  {
-    name: "Posh",
-    fileType: "png",
-    link: "https://www.dawnzer.po.sh",
-    bgColor: "#ffffff"
-  },
-  {
-    name: "UTSA_SDS",
-    fileType: "png",
-    link: "https://provost.utsa.edu/sds/",
-    bgColor: "#212529"
-  },
-  {
-    name: "UTSA_Research",
-    fileType: "png",
-    link:
-      "https://www.utsa.edu/president/organization/vp-search/VPREDKE-Search.html",
-    bgColor: "#ffffff"
-  },
-  {
-    name: "NSA",
-    fileType: "png",
-    link: "https://www.nsa.gov/",
-    bgColor: "#ffffff"
-  }
+	{
+		name: 'UTSA_COS',
+		fileType: 'png',
+		link: 'https://www.utsa.edu/sciences/',
+		bgColor: '#ffffff'
+	},
+	{
+		name: 'Valero',
+		fileType: 'png',
+		link: 'https://www.valero.com/en-us',
+		bgColor: '#ffffff'
+	},
+	{
+		name: 'Webhead',
+		fileType: 'png',
+		link: 'https://www.webheadtech.com/',
+		bgColor: '#ffffff'
+	},
+	{
+		name: 'Posh',
+		fileType: 'png',
+		link: 'https://www.dawnzer.po.sh',
+		bgColor: '#ffffff'
+	},
+	{
+		name: 'UTSA_SDS',
+		fileType: 'png',
+		link: 'https://provost.utsa.edu/sds/',
+		bgColor: '#212529'
+	},
+	{
+		name: 'UTSA_Research',
+		fileType: 'png',
+		link: 'https://www.utsa.edu/president/organization/vp-search/VPREDKE-Search.html',
+		bgColor: '#ffffff'
+	},
+	{
+		name: 'NSA',
+		fileType: 'png',
+		link: 'https://www.nsa.gov/',
+		bgColor: '#ffffff'
+	}
 ];
 
-const otherPartners = [{ name: "MLH", link: "https://mlh.io/" }];
+const otherPartners = [ { name: 'MLH', link: 'https://mlh.io/' } ];
 
 const PartnerContent: React.FC = () => {
-  return (
-    <PartnerContentWrapper>
-      <PartnerTitleWrapper>OUR PARTNERS</PartnerTitleWrapper>
-      <GoldPartnerSection />
-      <SilverPartnerSection />
-      <BronzePartnerSection />
-      <OtherPartnerSection />
-    </PartnerContentWrapper>
-  );
+	return (
+		<PartnerContentWrapper>
+			<PartnerTitleWrapper>OUR PARTNERS</PartnerTitleWrapper>
+			<GoldPartnerSection />
+			<SilverPartnerSection />
+			<BronzePartnerSection />
+			<OtherPartnerSection />
+		</PartnerContentWrapper>
+	);
 };
 
 const GoldPartnerSection: React.FC = () => {
-  return (
-    <GoldPartnerContainer>
-      {goldPartners.map(partner => {
-        const { name, link } = partner;
-        const padding =
-          partner.paddingTop === undefined ? 0 : partner.paddingTop;
-        return (
-          <a href={link} key={name + "-partner"}>
-            <div>
-              <GoldPartnerImage
-                key={name + "Image"}
-                src={require("../../static/PartnerLogos/" + name + ".png")}
-                alt={name}
-                theme={{ padding: padding }}
-              />
-            </div>
-          </a>
-        );
-      })}
-    </GoldPartnerContainer>
-  );
+	return (
+		<GoldPartnerContainer>
+			{goldPartners.map((partner) => {
+				const { name, link } = partner;
+				const padding = partner.paddingTop === undefined ? 0 : partner.paddingTop;
+				return (
+					<a href={link} key={name + '-partner'}>
+						<div>
+							<GoldPartnerImage
+								key={name + 'Image'}
+								src={require('../../static/PartnerLogos/' + name + '.png')}
+								alt={name}
+								theme={{ padding: padding }}
+							/>
+						</div>
+					</a>
+				);
+			})}
+		</GoldPartnerContainer>
+	);
 };
 
 const SilverPartnerSection: React.FC = () => {
-  return (
-    <SilverPartnerContainer>
-      {silverPartners.map(partner => {
-        const { name, link } = partner;
-        return (
-          <a href={link} key={name + "-partner"}>
-            <div>
-              <SilverPartnerImage
-                key={name + "Image"}
-                src={require("../../static/PartnerLogos/" + name + ".png")}
-                alt={name}
-              />
-            </div>
-          </a>
-        );
-      })}
-    </SilverPartnerContainer>
-  );
+	return (
+		<SilverPartnerContainer>
+			{silverPartners.map((partner) => {
+				const { name, link } = partner;
+				return (
+					<a href={link} key={name + '-partner'}>
+						<div>
+							<SilverPartnerImage
+								key={name + 'Image'}
+								src={require('../../static/PartnerLogos/' + name + '.png')}
+								alt={name}
+							/>
+						</div>
+					</a>
+				);
+			})}
+		</SilverPartnerContainer>
+	);
 };
 
 const BronzePartnerSection: React.FC = () => {
-  return (
-    <BronzePartnerContainer>
-      {bronzePartners.map(partner => {
-        const { name, fileType, link, bgColor } = partner;
-        return (
-          <a
-            href={link}
-            style={{ minWidth: "120px", margin: "0px 15px" }}
-            key={name + "-partner"}
-          >
-            <div>
-              <BronzePartnerImage
-                key={name + "Image"}
-                src={require("../../static/PartnerLogos/" +
-                  name +
-                  "." +
-                  fileType)}
-                alt={name}
-                theme={{ backgroundColor: bgColor }}
-              />
-            </div>
-          </a>
-        );
-      })}
-    </BronzePartnerContainer>
-  );
+	return (
+		<BronzePartnerContainer>
+			{bronzePartners.map((partner) => {
+				const { name, fileType, link, bgColor } = partner;
+				return (
+					<a href={link} style={{ minWidth: '120px', margin: '0px 15px' }} key={name + '-partner'}>
+						<div>
+							<BronzePartnerImage
+								key={name + 'Image'}
+								src={require('../../static/PartnerLogos/' + name + '.' + fileType)}
+								alt={name}
+								theme={{ backgroundColor: bgColor }}
+							/>
+						</div>
+					</a>
+				);
+			})}
+		</BronzePartnerContainer>
+	);
 };
 
 const OtherPartnerSection: React.FC = () => {
-  return (
-    <OtherPartnerContainer>
-      {otherPartners.map(partner => {
-        const { name, link } = partner;
-        return (
-          <a href={link} key={name + "-partner"}>
-            <div>
-              <OtherPartnerImage
-                key={name + "Image"}
-                src={require("../../static/PartnerLogos/" + name + ".jpg")}
-                alt={name}
-              />
-            </div>
-          </a>
-        );
-      })}
-    </OtherPartnerContainer>
-  );
+	return (
+		<OtherPartnerContainer>
+			{otherPartners.map((partner) => {
+				const { name, link } = partner;
+				return (
+					<a href={link} key={name + '-partner'}>
+						<div>
+							<OtherPartnerImage
+								key={name + 'Image'}
+								src={require('../../static/PartnerLogos/' + name + '.jpg')}
+								alt={name}
+							/>
+						</div>
+					</a>
+				);
+			})}
+		</OtherPartnerContainer>
+	);
 };
 
 export default PartnerSection;
--- src/sections/Partners/PartnerStyle.tsx
+++ src/sections/Partners/PartnerStyle.tsx
@@ -1,123 +1,123 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const GoldPartnerImage = styled.img`
-  max-height: 230px;
-  max-width: 440px;
-  margin: 10px;
-  margin-top: 50px;
-  padding-top: ${props => props.theme.padding}px;
-  @media (max-width: 800px) {
-    max-height: 115px;
-    max-width: 220px;
-    margin: 10px;
-    padding-top: ${props => {
-      return props.theme.padding / 2;
-    }}px;
-  }
+	max-height: 230px;
+	max-width: 440px;
+	margin: 10px 30px;
+	margin-top: 50px;
+	padding-top: ${(props) => props.theme.padding}px;
+	@media (max-width: 800px) {
+		max-height: 115px;
+		max-width: 220px;
+		margin: 10px;
+		padding-top: ${(props) => {
+			return props.theme.padding / 2;
+		}}px;
+	}
 `;
 
 const GoldPartnerContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  margin-bottom: 50px;
-  @media (max-width: 800px) {
-    margin-bottom: 15px;
-  }
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+	margin-bottom: 50px;
+	@media (max-width: 800px) {
+		margin-bottom: 15px;
+	}
 `;
 
 const SilverPartnerImage = styled.img`
-  max-height: 190px;
-  max-width: 370px;
-  margin: 10px;
-  margin-top: 50px;
-  @media (max-width: 800px) {
-    margin: 15px 10px;
-    max-height: 80px;
-    max-width: 180px;
-  }
+	max-height: 190px;
+	max-width: 370px;
+	margin: 10px 20px;
+	margin-top: 50px;
+	@media (max-width: 800px) {
+		margin: 15px 10px;
+		max-height: 80px;
+		max-width: 180px;
+	}
 `;
 
 const SilverPartnerContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  flex-wrap: wrap;
-  @media (max-width: 800px) {
-    margin: 0px 10px 15px;
-  }
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+	flex-wrap: wrap;
+	@media (max-width: 800px) {
+		margin: 0px 10px 15px;
+	}
 `;
 
 const BronzePartnerImage = styled.img`
-  max-height: 120px;
-  min-height: 30px;
-  min-width: 120px;
-  max-width: 320px;
-  margin: 5px;
-  margin-top: 50px;
-  background-color: ${props => props.theme.backgroundColor};
-  padding: 5px;
-  @media (max-width: 800px) {
-    max-height: 60px;
-    max-width: 140px;
-    min-height: 20px;
-    min-width: 0px;
-    margin: 10px 5px;
-  }
+	max-height: 120px;
+	min-height: 30px;
+	min-width: 120px;
+	max-width: 320px;
+	margin: 5px;
+	margin-top: 50px;
+	background-color: ${(props) => props.theme.backgroundColor};
+	padding: 5px;
+	@media (max-width: 800px) {
+		max-height: 60px;
+		max-width: 140px;
+		min-height: 20px;
+		min-width: 0px;
+		margin: 10px 5px;
+	}
 `;
 
 const BronzePartnerContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  margin: 50px 10px 10px;
-  max-width: 1500px;
-  @media (max-width: 800px) {
-    margin: 15px;
-  }
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	flex-wrap: wrap;
+	margin: 50px 10px 10px;
+	max-width: 1500px;
+	@media (max-width: 800px) {
+		margin: 15px;
+	}
 `;
 
 const PartnerContentWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
+	display: flex;
+	flex-wrap: wrap;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
 `;
 
 const PartnerTitleWrapper = styled.div`
-  font-size: 55px;
-  font-weight: 400;
-  font-family: Arial-Black;
-  margin-bottom: 30px;
-  @media (max-width: 800px) {
-    font-size: 42px;
-  }
+	font-size: 55px;
+	font-weight: 400;
+	font-family: Arial-Black;
+	margin-bottom: 30px;
+	@media (max-width: 800px) {
+		font-size: 42px;
+	}
 `;
 
 const OtherPartnerContainer = styled.div``;
 
 const OtherPartnerImage = styled.img`
-  max-height: 200px;
-  margin: 10px;
-  @media (max-width: 800px) {
-    max-height: 100px;
-  }
+	max-height: 200px;
+	margin: 10px;
+	@media (max-width: 800px) {
+		max-height: 100px;
+	}
 `;
 
 export {
-  GoldPartnerImage,
-  SilverPartnerImage,
-  BronzePartnerImage,
-  OtherPartnerImage,
-  PartnerContentWrapper,
-  PartnerTitleWrapper,
-  GoldPartnerContainer,
-  SilverPartnerContainer,
-  BronzePartnerContainer,
-  OtherPartnerContainer
+	GoldPartnerImage,
+	SilverPartnerImage,
+	BronzePartnerImage,
+	OtherPartnerImage,
+	PartnerContentWrapper,
+	PartnerTitleWrapper,
+	GoldPartnerContainer,
+	SilverPartnerContainer,
+	BronzePartnerContainer,
+	OtherPartnerContainer
 };
