a4552a193b3afea4781c5f237d8d45905517e4a6
LeonY1 <leonyyuan@gmail.com>
2020-03-23 02:22:05 -0500

    Adding Schedule Section changes

--- src/sections/Schedule/ScheduleSection.tsx
+++ src/sections/Schedule/ScheduleSection.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import SectionBlock from "../../components/SectionDividers/SectionBlock";
+import { ScheduleConstants } from "../SectionConstant";
+import { ScheduleBlock, ScheduleDate, ScheduleEvent } from "./ScheduleStyle";
 
 function ScheduleSection() {
   return (
@@ -7,15 +9,42 @@ function ScheduleSection() {
       {{
         content: () => {
           return (
-            <>
+            <div>
               <div>Schedule</div>
-              <div>Coming Soon!</div>
-            </>
+              <Schedule />
+            </div>
           );
         }
       }}
     </SectionBlock>
   );
 }
+
+const Schedule: React.FC = () => {
+  const { days } = ScheduleConstants;
+  return (
+    <ScheduleBlock>
+      {days.map(({ date, events }) => {
+        return (
+          <div>
+            <ScheduleDate>{date}</ScheduleDate>
+
+            <ul>
+              {events.map(({ title, time, location }) => {
+                return (
+                  <ScheduleEvent>
+                    <div>{title}</div>
+                    <div>{time}</div>
+                    <div>{location}</div>
+                  </ScheduleEvent>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </ScheduleBlock>
+  );
+};
 
 export default ScheduleSection;
