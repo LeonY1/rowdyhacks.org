7c038c6dd9b75af4b6879bc3df44c6594e23cbc5
LeonY1 <leonyyuan@gmail.com>
2020-03-23 22:17:40 -0500

    Finalizing Schedule Section

--- src/sections/Schedule/ScheduleStyle.tsx
+++ src/sections/Schedule/ScheduleStyle.tsx
@@ -1,22 +1,66 @@
 import styled from "styled-components";
 
-const ScheduleBlock = styled.div`
-  display: flex;
-  justify-content: center;
+const ScheduleTableDiv = styled.div`
+  overflow: hidden auto;
+  height: 632px;
+  word-wrap: break-word;
+  background-color: #444;
+  border-radius: 2px;
+  margin-bottom: 70px;
+  border: 2px solid #444;
 `;
 
-const ScheduleDate = styled.h3`
+const ScheduleBlock = styled.table`
+  table-layout: auto;
+  border-style: none;
+  border-collapse: collapse;
+`;
+
+const ScheduleDate = styled.tr`
   font-family: Arial-Black;
-  color: #034872;
+  color: #ffffff;
+  width: parent;
+  border-top: 1px solid #333333;
+  height: 40px;
 `;
 
-const ScheduleEvent = styled.li`
-  list-style-type: none;
+const ScheduleEvent = styled.tr`
   text-align: left;
   margin: 10px 5px;
-  max-width: 350px;
-  color: #034872;
+  max-width: 400px;
+  color: #ffffff;
+  background-color: #027380;
   font-family: Helvetica;
+  border-top: 1px solid #333333;
+  padding: 100px;
+  height: 45px;
 `;
 
-export { ScheduleBlock, ScheduleDate, ScheduleEvent };
+const ScheduleEventTitle = styled.td``;
+
+const ScheduleEventTime = styled.td`
+  width: 100px;
+  padding-left: 10px;
+`;
+
+const ScheduleEventLocation = styled.td`
+  max-width: 300px;
+  text-align: right;
+`;
+
+const ScheduleTitle = styled.div`
+  font-size: 55px;
+  font-family: Arial-Black;
+  margin-bottom: 30px;
+  color: #4d4d4d;
+`;
+export {
+  ScheduleBlock,
+  ScheduleDate,
+  ScheduleEvent,
+  ScheduleEventTitle,
+  ScheduleEventTime,
+  ScheduleEventLocation,
+  ScheduleTitle,
+  ScheduleTableDiv
+};
